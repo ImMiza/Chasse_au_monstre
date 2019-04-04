84169011e0ccf594f47faecfea61e64a7e27cfd1
Adrien-Ba <45963338+Adrien-Ba@users.noreply.github.com>
2019-04-04 16:09:28 +0200

    ajout cases & perso
    Création des cases & personnages

---
+++ src/cam/utils/cases/TeleportCase.java
@@ -0,0 +1,8 @@
+package cam.utils.cases;
+
+public class TeleportCase extends Case {
+	//ATTRIBUTS
+	public void test() {
+	}
+
+}
---
+++ src/cam/utils/personnages/Position.java
@@ -0,0 +1,53 @@
+package cam.utils.personnages;
+
+/**
+ * Class qui permet de créer une position relative à un objet.
+ * @author Aymeric DEFOSSEZ
+ *
+ */
+public class Position {
+
+	private int x, y;
+	
+	/**
+	 * Constructeur qui prend en paramètre 2 int pour créer une position.
+	 * @param x : 1ère coordonnée.
+	 * @param y : 2nde coordonnée.
+	 */
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Retourne la 1ère coordonnée de la position.
+	 * @return la 1ère coordonnée de la position.
+	 */
+	public int getX() {
+		return x;
+	}
+	
+	/**
+	 * Permet de modifier la 1ère coordonnée de la position.
+	 * @param x : Nouvelle coordonnée.
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	/**
+	 * Retourne la 2nde coordonnée de la position.
+	 * @return la 2nde coordonnée de la position.
+	 */
+	public int getY() {
+		return y;
+	}
+	
+	/**
+	 * Permet de modifier la 2nde coordonnée de la position.
+	 * @param y : Nouvelle coordonnée.
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+}
