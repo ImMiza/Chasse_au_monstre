a74b34f72dbd1c247fd06194ea60b7d57eff6d31
theteksa <aymeric03d@gmail.com>
2019-04-29 08:44:27 +0200

    test plateau

--- src/cam/utils/tests/TestPlateau.java
+++ src/cam/utils/tests/TestPlateau.java
@@ -1,10 +1,16 @@
 package cam.utils.tests;
 
+import cam.utils.personnages.Chasseur;
+import cam.utils.personnages.Monstre;
 import cam.utils.plateau.Plateau;
 
 public class TestPlateau {
 
 	public static void main(String[] args) {
-		Plateau p;
+		Monstre m = new Monstre(5, 5, "Kevin");
+		Chasseur c = new Chasseur(0, 0, "Pierre");
+		Plateau p = new Plateau(m, c, 10, 10);
+		
+		p.printPlateau();
 	}
 }
