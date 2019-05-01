2a661d45ebf13ce1ee6ee707f17f2b5e17db50d6
allan <allan150199@outlook.fr>
2019-05-01 16:53:47 +0200

    Update TestPlateau.java

--- src/cam/utils/tests/TestPlateau.java
+++ src/cam/utils/tests/TestPlateau.java
@@ -1,5 +1,7 @@
 package cam.utils.tests;
 
+import cam.utils.cases.Case;
+import cam.utils.cases.SlowCase;
 import cam.utils.personnages.Chasseur;
 import cam.utils.personnages.Monstre;
 import cam.utils.plateau.Plateau;
@@ -7,12 +9,31 @@ import cam.utils.plateau.Plateau;
 public class TestPlateau {
 
 	public static void main(String[] args) {
+		Case[][] cases = new Case[10][10];
+		
+		for(int i = 0; i < cases.length; i++) {
+			for(int j = 0; j < cases.length; j++) {
+				cases[i][j] = initCase();
+			}
+		}
 		Monstre m = new Monstre(5, 5, "Kevin");
 		Chasseur c = new Chasseur(1, 1, "Pierre");
-		Plateau p = new Plateau(m, c, 10, 10);
+		Plateau p = new Plateau(m, c, cases);
 		
 		p.printPlateau(true);
+		System.out.println("=======");
+		
 		p.printPlateau(false);
+		System.out.println("=======");
+		
 		p.printPlateauDebug();
+	}
+	
+	public static Case initCase() {
+		if (Math.random() < 0.10) {
+			return new SlowCase();
+		} else {
+			return new Case();
+		}
 	}
 }
