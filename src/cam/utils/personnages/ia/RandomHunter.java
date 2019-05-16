e642e36ad4712339d1e91bd3dfd2b8cd5aa36a20
Adrien-Ba <45963338+Adrien-Ba@users.noreply.github.com>
2019-05-16 16:01:31 +0200

    Update RandomHunter.java

--- src/cam/utils/personnages/ia/RandomHunter.java
+++ src/cam/utils/personnages/ia/RandomHunter.java
@@ -6,6 +6,10 @@ import cam.utils.personnages.Position;
 import cam.utils.plateau.Plateau;
 
 public class RandomHunter extends ChasseurIA{
+	//ATTRIBUTS
+	
+	
+	
 	//CONSTRUCTEURS
 	public RandomHunter(int x, int y, String nom) {
 		super(x, y, nom);
@@ -26,6 +30,7 @@ public class RandomHunter extends ChasseurIA{
 		int maxY = plateau.getPlateau()[0].length;
 		int y = m.nextInt(maxY);
 		return new Position(x, y);
+		//AJOUTER LA DETECTION DE DOUBLONS
 	}
 
 	@Override
