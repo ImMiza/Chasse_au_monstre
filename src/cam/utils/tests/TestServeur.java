150343669c99121f4b02ed1dceb284d8f6d9d424
Mizaki <allan150199@outlook.fr>
2019-05-28 10:10:59 +0200

    Remove Warning

--- src/cam/utils/tests/TestServeur.java
+++ src/cam/utils/tests/TestServeur.java
@@ -1,7 +1,6 @@
 package cam.utils.tests;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import cam.utils.net.InputListener;
 import cam.utils.net.OutputListener;
@@ -12,17 +11,10 @@ public class TestServeur {
 	public static void main(String[] args) {
 
 		try {
-<<<<<<< HEAD
-			Server server = new Server(5007);
-			server.listen();
-			
-			System.out.println("connecte");
-=======
 			Server server = new Server(5006);
 			server.listen();
 			
 			System.out.println("Client connecté");
->>>>>>> d49b95843b1efc1d67f50a208ef43044f039f2ae
 			Thread output = new Thread(new OutputListener(server));
 			Thread input = new Thread(new InputListener(server));
 			
