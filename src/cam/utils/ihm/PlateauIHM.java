1e6782f577cb90423862d71adaf11151ad96f31f
theteksa <aymeric03d@gmail.com>
2019-05-29 10:38:05 +0200

    plateauIHM

---
+++ src/cam/utils/ihm/PlateauIHM.java
@@ -0,0 +1,11 @@
+package cam.utils.ihm;
+
+public class PlateauIHM {
+
+	private static CaseIHM[][] lesCases;
+	
+	public PlateauIHM(int nbLig, int nbCol) {
+		lesCases = new CaseIHM[nbLig][nbCol];
+	}
+	
+}
