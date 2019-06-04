c729450aa874bc1d0e4ac9756d49534b74c79e42
Hugo <hugo.wieder@gmail.com>
2019-06-04 10:37:02 +0200

    s

--- src/cam/utils/ihm/menu/events/PierreFeuilleCiseauxEvent.java
+++ src/cam/utils/ihm/menu/events/PierreFeuilleCiseauxEvent.java
@@ -6,6 +6,67 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 public class PierreFeuilleCiseauxEvent {
+    /**
+     * 1 = Pierre
+     * 2 = Feuille
+     * 3 = Ciseaux
+     */
+    private int choixMonstre;
+    private int choixChasseur ;
+
+    /**
+     * Setter de choixChasseur
+     * @param choixChasseur
+     */
+    public void setChoixChasseur(int choixChasseur) {
+        this.choixChasseur = choixChasseur;
+    }
+
+    /**
+     * Setter de choixMonstre
+     * @param choixMonstre
+     */
+    public void setChoixMonstre(int choixMonstre) {
+        this.choixMonstre = choixMonstre;
+    }
+
+    /**
+     * Getter de choixChasseur
+     * @return
+     */
+    public int getChoixChasseur() {
+        return choixChasseur;
+    }
+
+    /**
+     * Getter de choixMonstre
+     * @return
+     */
+    public int getChoixMonstre() {
+        return choixMonstre;
+    }
+
+    /**
+     * Donne le resultat du pierre feuille ciseaux
+     * @return -1 si le monstre gagne
+     *  0 si il y a une egalite
+     *  1 si le chasseur a gagne
+     */
+    public int getResult(){
+        if (choixChasseur == 1) {
+            if(choixMonstre == 1) return 0;
+            else if(choixMonstre == 2) return -1;
+            else return 1;
+        } else if (choixChasseur == 2){
+            if (choixMonstre == 1) return 1;
+            else if (choixMonstre == 2) return 0;
+            else return -1;
+        } else {
+            if (choixMonstre == 1) return -1;
+            else if (choixMonstre == 2) return 1;
+            else return 0;
+        }
+    }
 
     @FXML
     private Button btnCiseaux;
@@ -19,6 +80,7 @@ public class PierreFeuilleCiseauxEvent {
     @FXML
     void pierreChoix(MouseEvent event) {
     	System.out.println("Pierre");
+
     }
 
     @FXML
@@ -29,6 +91,10 @@ public class PierreFeuilleCiseauxEvent {
     @FXML
     void ciseauxChoix(MouseEvent event) {
     	System.out.println("Ciseaux");
+    }
+
+    public void initialize(){
+
     }
 
 }
