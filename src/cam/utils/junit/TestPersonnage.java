d7a32fee84bf1c07d92d47ae5f08a1fa55a89237
allan <allan150199@outlook.fr>
2019-06-12 22:28:17 +0200

    update JUnit

--- src/cam/utils/junit/TestPersonnage.java
+++ src/cam/utils/junit/TestPersonnage.java
@@ -1,44 +1,45 @@
 package cam.utils.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import cam.utils.personnages.Personnage;
 
-class TestPersonnage {
+public class TestPersonnage
+{
 
 	private static Personnage p;
 	
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUpBeforeClass() {
 		p = new Personnage(2, 5, "test");
 	}
 
 	@Test
-	void testNom1() {
+	public void testNom1() {
 		assertEquals(p.getNom(), "test");
 	}
 
 	@Test
-	void testNom2() {
+	public void testNom2() {
 		p.setNom("nouveau test");
 		assertEquals(p.getNom(), "nouveau test");
 	}
 	
 	@Test
-	void testPosition1() {
+	public void testPosition1() {
 		assertEquals(p.getPosition().getX(), 2);
 		assertEquals(p.getPosition().getY(), 5);
 	}
 	
 	@Test
-	void testPosition2() {
+	public void testPosition2() {
 		p.setX(6);
 		p.setY(3);
 		assertEquals(p.getPosition().getX(), 6);
 		assertEquals(p.getPosition().getY(), 3);
 	}
-	
+
 }
--- src/cam/utils/junit/TestPlateau.java
+++ src/cam/utils/junit/TestPlateau.java
@@ -1,39 +1,53 @@
 package cam.utils.junit;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
 import cam.utils.ihm.CaseIHM;
 import cam.utils.ihm.PlateauIHM;
 import cam.utils.personnages.Chasseur;
 import cam.utils.personnages.Monstre;
+import cam.utils.personnages.Position;
 
-class TestPlateau {
+public class TestPlateau
+{
 
-	private static PlateauIHM p;
-	private static Monstre m;
-	private static Chasseur c;
-	private static CaseIHM[][] cases;
-	
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		m = new Monstre(2, 2, "monstre");
+	private static PlateauIHM	p;
+	private static Monstre		m;
+	private static Chasseur		c;
+	private static CaseIHM[][]	cases;
+
+	@Before
+	public void before()
+	{
+		m = new Monstre(0, 0, "monstre");
 		c = new Chasseur(6, 4, "chasseur");
 		cases = new CaseIHM[10][10];
-		for (int i = 0; i < cases.length; i++) {
-			for (int j = 0; j < cases[i].length; j++) {
-				cases[i][j] = new CaseIHM(i, j, 50 * i, 50 * j, 50, 50);
+		for (int i = 0; i < cases.length; i++)
+		{
+			for (int j = 0; j < cases[i].length; j++)
+			{
+				cases[i][j] = new CaseIHM(i, j, 0, 0, 0, 0);
 			}
 		}
-		p = new PlateauIHM(m , c, cases);
+		p = new PlateauIHM(m, c, cases);
 	}
 
 	@Test
-	void testDeplacerMonstre() {
-		assertEquals(p.getMonstre(), m);
+	public void testDeplacerMonstre()
+	{
+		assertTrue(p.deplacerMonstre(cases[0][1]));
+		
+		Position pos = new Position(0, 1);
+		assertEquals(p.getMonstre().getPosition().getX(), pos.getX());
+		assertEquals(p.getMonstre().getPosition().getY(), pos.getY());
+		
+		assertTrue(p.deplacerMonstre(cases[1][1]));
+		assertNotEquals(p.getMonstre().getPosition().getX(), pos.getX());
+		
+		assertFalse(p.deplacerMonstre(cases[5][5]));
 	}
 
 }
